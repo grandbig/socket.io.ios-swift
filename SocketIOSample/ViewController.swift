1654ec0485cb2d5a410c39e973f1dba459f5a760
加藤 雄大 <takahiro.kato15@gmail.com>
2015-03-11 14:27:12 +0900

    emit処理の実装
    - Storyboardにテキストフィールドとボタンを設置
    - ボタン押下時にテキストフィールド内のテキストをWebSocketでサーバに送る処理を実装
    - キーボードのreturnキー押下時にキーボードを閉じる処理を実装

--- SocketIOSample/ViewController.swift
+++ SocketIOSample/ViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var msgField: UITextField!
 
-    let socket = SocketIOClient(socketURL: "172.20.10.5:3000")
+    let socket = SocketIOClient(socketURL: "localhost:3000")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Delegateの設定
+        self.msgField.delegate = self
         
         // コネクションの接続
         self.socket.connect();
@@ -25,6 +29,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // キーボードのreturnを押下するとキーボードが閉じる処理
+        self.msgField.resignFirstResponder()
+        
+        return true
+    }
+    
+    @IBAction func sendMessage(sender: AnyObject) {
+        // テキストフィールド内の文字列をWebSocketで送る処理
+        var msg = self.msgField.text
+        self.socket.emit("chat message", msg)
+    }
 
 }
 
